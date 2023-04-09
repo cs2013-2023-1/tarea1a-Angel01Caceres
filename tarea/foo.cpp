58124122940d2c606acae791ce531774434fa855
Angel01Caceres <95986062+Angel01Caceres@users.noreply.github.com>
2023-04-09 18:57:38 -0500

    Primer Commit

--- tarea/foo.cpp
+++ tarea/foo.cpp
@@ -1,27 +1,65 @@
 #include "foo.h"
 
+void matriz(float** ptr,filas,columnas) {
+    ptr = new float*[filas]
+    for (int i = 0; i < filas; i++)
+        ptr[i] = new float[columnas];
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; i < columnas; i++)
+            ptr[i][j] = (rand() % 100)/100;
+    }
+}
+
 Matriz2D::Matriz2D(){
     // Constructor por defecto
+    filas = columnas = 3;
+    matriz(ptr,filas,columnas);
 }
 
-Matriz2D::Matriz2D(int n){
+Matriz2D::Matriz2D(int n) {
     // Constructor con un parametro
+    filas = columnas = n;
+    matriz(ptr,filas,columnas);
 }
 
-Matriz2D::Matriz2D(int n, int m){
+Matriz2D::Matriz2D(int n, int m) {
     // Constructor con dos parametros
+    filas = n; columnas = m;
+    matriz(ptr,filas,columnas);
 }
 
 Matriz2D::Matriz2D(const Matriz2D& m){
     // Constructor de copia
+    this->filas = m.filas;
+    this->columnas = m.columnas;
+    ptr = new float*[filas];
+    for (int i = 0; i < filas; i++)
+        ptr[i] = new float[columnas];
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; i < columnas; i++)
+            ptr[i][j] = m.ptr[i][j];
+    }
 }
 
 Matriz2D::Matriz2D(Matriz2D&& m){
     // Constructor de movimiento
+    this->filas = m.filas;
+    this->columnas = m.columnas;
+    ptr = m.ptr;
+    m.ptr = nullptr;
 }
 
 Matriz2D t(Matriz2D& m){
     // Transpuesta de una matriz
+    this->filas = m.columnas;
+    this->columnas = m.filas;
+    ptr = new float*[filas];
+    for (int i = 0; i < filas; i++)
+        ptr[i] = new float[columnas];
+    for (int i = 0; i < filas; i++) {
+        for (int j = 0; i < columnas; i++)
+            ptr[i][j] = m.ptr[j][i];
+    }
 }
 
 std::ostream& operator<<(std::ostream& os, const Matriz2D& m){
