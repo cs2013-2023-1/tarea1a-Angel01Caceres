92720440bd9d56041956c1791c0e960f3ad9e2ff
Angel01Caceres <95986062+Angel01Caceres@users.noreply.github.com>
2023-04-09 20:59:19 -0500

    Septimo Commit

--- tarea/foo.cpp
+++ tarea/foo.cpp
@@ -56,25 +56,23 @@ Matriz2D::Matriz2D(Matriz2D&& m){
     m.ptr = nullptr;
 }
 
+Matriz2D::Matriz2D(int filas,int columnas,float** ptr){
+    // Constructor por elementos
+    this->filas = filas;
+    this->columnas = columnas;
+    this->ptr = ptr;
+    ptr = nullptr;
+}
+
 Matriz2D t(Matriz2D& m){
     // Transpuesta de una matriz
-    float** ptr1 = m.ptr;
-    float cantidad;
-    m.ptr = nullptr;
-    cantidad = m.filas;
-    m.filas = m.columnas;
-    m.columnas = cantidad;
-    m.ptr = matriz(m.filas,m.columnas);
-    for (int i = 0; i < m.filas; i++)  
-       { for (int j = 0; i < m.columnas; i++)
-            m.ptr[i][j] = ptr1[i][j]; }
-    Matriz2D m1 = move(m);
-    m.ptr = ptr1;
-    ptr1 = nullptr;
-    cantidad = m.filas;
-    m.filas = m.columnas;
-    m.columnas = cantidad;
-    return m1;
+    float** ptr = matriz(m.columnas,m.filas);
+    for (int i = 0; i < m.columnas; i++) {
+        for (int j = 0; i < m.filas; i++)
+            ptr[i][j] = m.ptr[i][j];
+    }
+    Matriz2D mt(m.columnas,m.filas,ptr);
+    return mt;
 }
 
 std::ostream& operator<<(std::ostream& os, const Matriz2D& m){
